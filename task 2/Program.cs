6e47a4b0840b957608d0a684ea629f32987d60f7
Ira88828 <vovik1112111@gmail.com>
2024-02-01 00:44:37 +0300

    commited program

---
+++ task 2/Program.cs
@@ -0,0 +1,16 @@
+﻿{
+  
+    {
+        int[] array = { 1, 2, 5, 0, 10, 34 };
+        ReverseArray(array, array.Length - 1);
+    }
+
+     void ReverseArray(int[] array, int index)
+    {
+        if (index >= 0)
+        {
+            Console.WriteLine(array[index]);
+            ReverseArray(array, index - 1);
+        }
+    }
+}
